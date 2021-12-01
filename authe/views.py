26cdf680f96c89a3058239c2f8b377a15c2ae598
kwon, minho <minhob38@gmail.com>
2021-12-01 22:12:30 +0900

    update get:user router

--- authe/views.py
+++ authe/views.py
@@ -19,7 +19,7 @@ def signup(request):
             is_user = User.objects.filter(email=email).exists()
             print(User.objects.all())
             if is_user:
-                data = {"status": "error", "message": "user already exists"}
+                data = { "status": "error", "message": "user already exists" }
                 return HttpResponseBadRequest(json.dumps(data), content_type="application/json")
             else:
                 salt = bcrypt.gensalt()
@@ -31,15 +31,31 @@ def signup(request):
                 print(JWT_SECRET_KEY)
                 return HttpResponse(json.dumps(data), content_type="application/json")
     except Exception as e:
-        data = {"status": "error", "message": str(e)}
+        data = { "status": "error", "message": str(e) }
         return HttpResponseServerError(json.dumps(data), content_type="application/json")
 
 def users(request):
     try:
         if request.method == "GET":
-            users = json.loads(serializers.serialize("json", User.objects.all()))
-            data = {"status": "success", "message": "found users",  "data": users}
-            return HttpResponse(json.dumps(data), content_type="application/json")
+            case = 3
+            if case == 1:
+                # queryest -> json #1
+                users = json.loads(serializers.serialize("json", User.objects.all()))
+                data = {"status": "success", "message": "found users",  "data": users}
+                return HttpResponse(json.dumps(data), content_type="application/json")
+            elif case == 2:
+                # queryest -> json #2
+                users = User.objects.all().values("email")
+                data = { "status": "success", "message": "found users",  "data": list(users) }
+                return HttpResponse(json.dumps(data), content_type="application/json")
+            else:
+                # queryest -> json #3
+                users = [{
+                    "email": user["email"],
+                    "created_at": user["created_at"].strftime("%Y-%m-%d")
+                } for user in User.objects.all().values()]
+                data = { "status": "success", "message": "found users",  "data": users }
+                return HttpResponse(json.dumps(data), content_type="application/json")
     except Exception as e:
-        data = {"status": "error", "message": str(e)}
+        data = { "status": "error", "message": str(e) }
         return HttpResponseServerError(json.dumps(data), content_type="application/json")

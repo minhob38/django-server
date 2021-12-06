2de583d34a3e85e2a0a6463413a664f4212d87d0
kwon, minho <minhob38@gmail.com>
2021-12-07 00:09:02 +0900

    change venv.sh

--- script.sh
+++ script.sh
@@ -7,5 +7,7 @@ python manage.py migrate
 python manage.py inspectdb
 python manage.py inspectdb --database=postgresql
 
+pip freeze > requirements.txt
+
 shp2pgsql -s 5179 -W euc-kr /Users/minho/Desktop/django-server/data/seoul_bjd/LSMD_ADM_SECT_UMD_11.shp seoul_bjd| psql -h localhost -p 5432 -U postgres -d study
 shp2pgsql -s 5179 -W euc-kr /Users/minho/Desktop/django-server/data/seoul_sgg/LARD_ADM_SECT_SGG_11.shp seoul_sgg| psql -h localhost -p 5432 -U postgres -d study
